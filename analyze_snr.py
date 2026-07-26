830d388720ea3ccfb01785c652ff14de442a992f
Estiaq Ahmed <estiaq.ahmed71@gmail.com>
2026-07-26 16:01:04 +0300

    Sorting of BeiDou SNRs
    beidou_snr(line) sorts SNR values from each  signals and gives empty list from signals with no SNRs or PRNs
    
    AI Usage: AI was used to remove last signal ID before checksum. It also suggested to use try, except command  as a safety net against corrupted signal,and to remove invisible new line for unforeseen future cases. Code implementation was done by me with tutorials from YouTube.

--- analyze_snr.py
+++ analyze_snr.py
@@ -1,4 +1,5 @@
 #This is a simple Python project for oneNAV. It will read nmea log files from Beidou satellite and calculate average SNRs
+from symbol import continue_stmt
 
 
 def read_lines(path):       #Read all lines of the file as a list of strings
@@ -15,12 +16,41 @@ def epoch_time(line):
     seconds = raw_time[4:]
     return hours + ":" + minutes + ":" + seconds
 
+def beidou_snrs(line):
+    line = line.strip()     #remove invisible new line
+    if not line.startswith("$GBGSV"):
+        return[]            #non BeiDou lines return empty list
+
+    #Example: $GBGSV,2,1,06,11,,,45,27,,,46,29,,,44,30,,,47,5*7A
+    no_checksum = line.split("*")[0]    #Remove *7A checksum
+    fields = no_checksum.split(",")    #Make individual fields
+    data = fields[4:]   #Remove first 4 index to keep only snr related data
+
+    if len(data) % 4 == 1:
+        data = data[:-1]    #Remove the last item (signal ID)
+
+    snrs = []
+    for item in range(0, len(data), 4):
+        prn_snr = data[item:item + 4]   #Keep only prn ID and SNRs from group of four's
+
+        prn = prn_snr[0]    #Satellite ID
+        snr = prn_snr[3]    #SNR value
+
+        if prn == "" or snr == "":
+            continue                   #Skip block if PRN or SNR is absent
+
+        try:
+            snrs.append(int(snr))
+        except ValueError:
+            continue
+    return snrs
+
 def main():
     lines = read_lines("logs/example-1.nmea")
 
     for line in lines:
-        if line.startswith("$GNGGA"):
-            print (epoch_time(line))
+        if line.startswith("$GBGSV"):
+            print (beidou_snrs(line))
 
 
 if __name__ == '__main__':
