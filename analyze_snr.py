0882e0d6ae84b3e6bd3ebd630f26aa411366872d
Estiaq Ahmed <estiaq.ahmed71@gmail.com>
2026-07-26 14:57:53 +0300

    Calculate epoch time from GNGGA
    epoch_time(line) reads the time field from GNGGA message, takes only the raw time, formats it to HH:MM:SS.ss
    Previously used gngga count is removed since it's no longer needed.
    
    AI Usage: AI was used to explain a whole GNGGA signal. It helped me to figure out which field represents time.

--- analyze_snr.py
+++ analyze_snr.py
@@ -5,17 +5,22 @@ def read_lines(path):       #Read all lines of the file as a list of strings
     with open(path) as nmea_log:
         return nmea_log.readlines()
 
+def epoch_time(line):
+    fields = line.split(",")
+    raw_time = fields[1]    #Taking the first field as raw time data
+
+    #Partition them by hours, minutes and seconds
+    hours = raw_time[0:2]
+    minutes = raw_time[2:4]
+    seconds = raw_time[4:]
+    return hours + ":" + minutes + ":" + seconds
 
 def main():
     lines = read_lines("logs/example-1.nmea")
 
-    count_gngga = 0
     for line in lines:
         if line.startswith("$GNGGA"):
-            count_gngga+=1
-
-    print("Total lines:", len(lines))
-    print("GNGGA messages (epochs):", count_gngga)
+            print (epoch_time(line))
 
 
 if __name__ == '__main__':
